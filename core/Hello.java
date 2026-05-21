69a09abec2ca8080acc016f25980b512d35c2210
unknown <akshithayadavbathula27@gmail.com>
2026-05-21 11:41:07 +0530

    First commit

---
+++ core/Hello.java
@@ -0,0 +1,10 @@
+
+//Java is structured not simple that compared to python
+
+// JSHELL : It is used to do experiments
+// In java we use methods: we can perform some actions
+// print() belongs to System.out 
+
+// to compile a code javac
+//java filename
+
